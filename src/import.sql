dd9a50e29ee29f4982aa52a70dd1e950e1fb42e2
Ignacio Nicolás Negro Caino <nachonegrocaino@gmail.com>
2018-11-20 20:22:20 -0300

    force quote

--- src/import.sql
+++ src/import.sql
@@ -1,2 +1,2 @@
-\copy userTP FROM '../resources/Users.tsv'  DELIMITER E'\t' CSV HEADER;
-\copy badges FROM '../resources/Badges.tsv' DELIMITER E'\t' CSV HEADER;
+\copy userTP FROM '../resources/Users.tsv'  DELIMITER E'\t' CSV HEADER FORCE_QUOTE *;
+\copy badges FROM '../resources/Badges.tsv' DELIMITER E'\t' CSV HEADER FORCE_QUOTE *;
